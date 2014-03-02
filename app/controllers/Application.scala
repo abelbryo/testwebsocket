48bcb1e6bd43c377772b32a632068ac699898614
Abel Terefe <abelbryo@gmail.com>
2014-03-02 12:20:00 +0200

    Added New websocket method

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,21 +1,24 @@
 package controllers
 
-import scala.concurrent._
-import ExecutionContext.Implicits.global
+import java.lang.management.ManagementFactory
 
 import play.api._
+import play.api.libs.concurrent.Promise
+import play.api.libs.iteratee._
 import play.api.mvc._
 
-import play.api.libs.iteratee._
-
+import scala.concurrent._
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 
 object Application extends Controller {
 
-  def index = WebSocket.using[String]{ request =>
+  def index = WebSocket.using[String] { request =>
     val in = Iteratee.foreach[String](println).map { msg =>
       println(msg)
-     }
-    val out = Enumerator("Hello") 
+    }
+    val out = Enumerator("Hello")
     (in, out)
   }
 
@@ -23,5 +26,22 @@ object Application extends Controller {
     Ok(views.html.index("Hello World"))
   }
 
+  def statusPage = Action { implicit request =>
+      Ok(views.html.statusPage(request))
+  }
+
+  def statusFeed() = WebSocket.using[String] { implicit request =>
+    def getLoadAverage = {
+      "%1.2f" format ManagementFactory.getOperatingSystemMXBean
+        .getSystemLoadAverage()
+    }
+
+    val in = Iteratee.ignore[String]
+    val out = Enumerator.repeatM {
+      Promise.timeout(getLoadAverage, 3 seconds)
+    }
+
+    (in, out)
+  }
 
 }
